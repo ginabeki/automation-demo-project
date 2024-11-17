72fec45b8ae747a1c64a117f0b3f9c1bd14b9c8f
ginabeki <ginabeki3@gmail.com>
2024-11-17 23:50:48 +0300

    test(setup): add initial test configuration and API tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,12 @@
-const app = require('../index');
-const request = require ('supertest');
+'use strict';
 
-describe ('API Tests', () => {
-    test('GET / should return hello message', async () => {
-        const response = await request(app).get('/');
-        ViolationOfExpectationsChain(response.body.message).toBe('Hello Github Actions!');
-        ViolationOfExpectationsChain(response.statusCode).toBe(200);
-    });
+const request = require('supertest');
+const app = require('../index');
+
+describe('API Tests', () => {
+  test('GET / should return hello message', async () => {
+    const response = await request(app).get('/');
+    expect(response.body.message).toBe('Hello Github Actions!');
+    expect(response.statusCode).toBe(200);
+  });
 });
